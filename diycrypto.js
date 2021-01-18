a5fedf3765bbaade70e36d0943fe776d0446523a
Dmannen03 <david.tornqvist@botkyrka.skola.se>
2021-01-18 14:47:54 +0100

    exported and imported functions

---
+++ diycrypto.js
@@ -0,0 +1,35 @@
+const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","å","ä","ö"," "];
+
+
+function letToNum (message){
+
+    return message.split("").map((char) => {
+        return letters.findIndex((letter) => char.toLowerCase() === letter);
+    });
+}
+
+function returnCesarNumbers(message,modifier){
+    
+
+    return letToNum(message).map((value) => {
+        if(value + modifier < letters.length)
+            return value + modifier;
+        else return value + modifier -letters.length;
+     
+            
+    });    
+
+}
+
+function numToLet (arr) {
+    return arr.map((value) => letters[value]);
+}
+
+
+export function caesarEncrypt (message, shift) {
+    return numToLet(returnCesarNumbers(message,shift)).join("");
+}
+
+export function caesarDecrypt(encryptedMessage, shift) {
+    return caesarEncrypt(encryptedMessage,-shift);
+}
--- main.js
+++ main.js
@@ -1,35 +1,4 @@
-const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","å","ä","ö"," "];
+import { caesarEncrypt, caesarDecrypt } from "./diycrypto.js"
 
-
-function letToNum (message){
-
-    return message.split("").map((char) => {
-        return letters.findIndex((letter) => char.toLowerCase() === letter);
-    });
-}
-
-function returnCesarNumbers(message,modifier){
-    
-
-    return letToNum(message).map((value) => {
-        if(value + modifier < letters.length)
-            return value + modifier;
-        else return value + modifier -letters.length;
-     
-            
-    });    
-
-}
-
-function numToLet (arr) {
-    return arr.map((value) => letters[value]);
-}
-
-
-function caesarEncrypt (message, shift) {
-    return numToLet(returnCesarNumbers(message,shift)).join("");
-}
-
-function caesarDecrypt(encryptedMessage, shift) {
-    return caesarEncrypt(encryptedMessage,-shift);
-}
+console.log(caesarEncrypt("hejsan",3));
+console.log(caesarDecrypt(caesarEncrypt("hejsan",3),3));
